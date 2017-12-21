9c6e223608e55443f8631b4ea8f12a54ab716ff2
Marc Aaron <marc.aaron.glasser@gmail.com>
2017-12-21 14:03:34 -0800

    get only

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const port = process.env.API_PORT || 3001;
 let db;
 
 // app.use(express.static('public'));
-
+//
 // app.get('/', (req, res) => res.sendFile('index.html'));
 
 
@@ -21,15 +21,15 @@ MongoClient.connect('mongodb://marcaaron:gm15410@ds044917.mlab.com:44917/ppl', f
 
 app.use(bodyParser.json());
 
-app.post('/', function(req, res) {
-   // Insert JSON straight into MongoDB
-  db.collection('exLists').insert(req.body, function (err, result) {
-      if (err)
-         res.send('Error');
-      else
-        res.send('Success');
-  });
-});
+// app.post('/', function(req, res) {
+//    // Insert JSON straight into MongoDB
+//   db.collection('exLists').insert(req.body, function (err, result) {
+//       if (err)
+//          res.send('Error');
+//       else
+//         res.send('Success');
+//   });
+// });
 
 app.get('/json', function(req, res){
 	db.collection('exLists').find({}).sort({_id:-1}).limit(6).toArray(function(err, workouts){
