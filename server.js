6c8a60a493b3a259d47003ec89972f5b7e557ed7
Marc Aaron <marc.aaron.glasser@gmail.com>
2018-01-27 19:12:57 -0800

    bypass cors

--- server.js
+++ server.js
@@ -23,6 +23,12 @@ MongoClient.connect(mongoURI, function(err, database){
 
 app.use(bodyParser.json());
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.post('/', function(req, res) {
    // Insert JSON straight into MongoDB
   db.collection('exLists').insert(req.body, function (err, result) {
